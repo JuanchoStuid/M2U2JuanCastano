50b566283f698a738252a0caf2aba47df09a970c
Juan David Castaño Gómez <juandcastag@gmail.com>
2022-06-26 17:30:16 -0500

    Actualización del valor total

--- ejercicio_uno/index.js
+++ ejercicio_uno/index.js
@@ -3,32 +3,61 @@ class Estudiante {
         this.nombre = nombre
     }
     mostrarNombre() {
-        let input_nombre = document.getElementById("nombre_estudiante").value;
+        var input_nombre = document.getElementById("nombre_estudiante").value;
         document.getElementById("mostrar_nombre").innerHTML = input_nombre;
 
-        let input_materias = document.getElementById("cantidad_materias").value;
+        var input_materias = document.getElementById("cantidad_materias").value;
         document.getElementById("mostrar_cantidad_materias").innerHTML = input_materias;
 
-        let papeleria = 20000;
-        let carne = 8000;
-        let descuento = 0.2;
+        var papeleria = 20000;
+        var carne = 8000;
+        var descuento = 0.2;
+        var subtotal_valor_materia = 0;
 
-        let total = papeleria + carne + descuento;
+        var tabla_valor_materia = document.getElementById("valor_materia").value;
+        // var tablita = document.getElementsByClassName("valores").value;
+        // console.log(tablita);
+        console.log(tabla_valor_materia);
+        // subtotal_valor_materia = subtotal_valor_materia + tabla_valor_materia;
+        // console.log(subtotal_valor_materia);
+        var total = papeleria + carne + descuento;
 
         document.getElementById("total").innerHTML = total;
 
+    }
+    mostrarTabla() {
+        var numFilas = document.getElementById("cantidad_materias").value;
+        var numColumnas = 2;
+        var contenedorTabla = document.getElementById("contenedorTabla");
+
+        contenedorTabla.innerHTML = "";
+        var tabla = "<table>";
+        tabla += "<tr><th>Materia</th><th>Valor</th></tr>";
+        for (var f = 1; f <= numFilas; f++) {
+            tabla += "<tr>";
+            // for (var c = 1; c <= numColumnas; c++) {
+            tabla += '<td><input type="text" id="nombre_materia" name="nombre_materia" value=""></td>';
+            tabla += '<td><input type="text" id="valor_materia" name="valor_materia" value="" class="valores"></td>';
+            // }
+            tabla += "</tr>";
+        }
+        tabla += "</table>";
+        contenedorTabla.innerHTML = tabla;
     }
 }
 
 const estudiante = new Estudiante("Estudiante");
 
-document.getElementById("boton").addEventListener("click", function(event) {
+document.getElementById("boton_calcular").addEventListener("click", function(event) {
 
     event.preventDefault();
     estudiante.mostrarNombre()
 
 });
 
-// const botonHTML = document.querySelector('#boton');
-// const nombreEstudianteHTML = document.querySelector('#nombre_estudiante');
-// botonHTML.addEventListener('click', estudiante.mostrarNombre);
+document.getElementById("boton_tabla").addEventListener("click", function(event) {
+
+    event.preventDefault();
+    estudiante.mostrarTabla()
+
+});
